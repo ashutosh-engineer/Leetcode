8fbd8c09eb7a6ae4a72b20005397623c7bd47995
ashutosh-engineer <ashutoshsingh6376@gmail.com>
2026-01-30 23:21:32 +0530

    Most optimiezed apporoach that is Dennis Flag algorithm

---
+++ leetcode75.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //Using The algorithm of DNF algorithm
+        int n=nums.size();
+        int mid=0,high=n-1,low=0;
+        while(mid <= high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                mid++;
+                low++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums[high],nums[mid]);
+                high--;
+            }
+        }
+    }
+};
